9f6bfc27bf3fa848236e2e9b7876b84c3b80f6cc
ysignat <ignatovegors@gmail.com>
2024-08-28 23:30:54 +0300

    fix(api): added some validation and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![forbid(unsafe_code)]
 #![warn(clippy::pedantic)]
+use anyhow::{anyhow, Context};
 use axum::{
     extract::Query,
     http::{HeaderMap, HeaderValue, StatusCode},
@@ -9,9 +10,12 @@ use axum::{
     Router,
 };
 use chrono::{NaiveDateTime, Utc};
+use errors::AppError;
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 use uuid::Uuid;
+
+mod errors;
 
 #[derive(Debug, Serialize)]
 struct Item {
@@ -30,13 +34,23 @@ struct UpdateItemBody {
 
 #[derive(Debug, Deserialize)]
 struct ListQueryParams {
-    page: Option<i32>,
-    limit: Option<i32>,
+    page: Option<u32>,
+    limit: Option<u32>,
 }
+
+const BIND_ADDRESS: &str = "0.0.0.0:8080";
+const PAGINATION_LIMIT_HEADER: &str = "Pagination-Limit";
+const DEFAULT_PAGINATION_LIMIT: u32 = 10;
+const PAGINATION_PAGE_HEADER: &str = "Pagination-Page";
+const DEFAULT_PAGINATION_PAGE: u32 = 1;
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = TcpListener::bind(BIND_ADDRESS)
+        .await
+        .context(format!("Cannot bind to {BIND_ADDRESS}"))
+        .unwrap();
+
     let router = Router::new()
         .route("/items", get(list_items).post(create_item))
         .route(
@@ -47,18 +61,37 @@ async fn main() {
     axum::serve(listener, router).await.unwrap();
 }
 
-async fn list_items(Query(list_params): Query<ListQueryParams>) -> impl IntoResponse {
+async fn list_items(
+    Query(list_params): Query<ListQueryParams>,
+) -> Result<impl IntoResponse, AppError> {
+    let header_value_error =
+        |k: &str, v: &str| format!("Cannot create valid value for header '{k}' from '{v}'");
+
     let mut headers = HeaderMap::new();
+
+    let page = list_params.page.unwrap_or(DEFAULT_PAGINATION_PAGE);
+    if page.eq(&0) {
+        Err(anyhow!("Page cannot be equal zero"))?;
+    }
+    let page_string = &page.to_string();
     headers.insert(
-        "Pagination-Page",
-        HeaderValue::from_str(&list_params.page.unwrap_or(1).to_string()).unwrap(),
-    );
-    headers.insert(
-        "Pagination-Limit",
-        HeaderValue::from_str(&list_params.limit.unwrap_or(10).to_string()).unwrap(),
+        PAGINATION_PAGE_HEADER,
+        HeaderValue::from_str(page_string)
+            .context(header_value_error(PAGINATION_PAGE_HEADER, page_string))?,
     );
 
-    (
+    let limit = list_params.limit.unwrap_or(DEFAULT_PAGINATION_LIMIT);
+    if limit.eq(&0) {
+        Err(anyhow!("Limit cannot be equal zero"))?;
+    }
+    let limit_string = &limit.to_string();
+    headers.insert(
+        PAGINATION_LIMIT_HEADER,
+        HeaderValue::from_str(limit_string)
+            .context(header_value_error(PAGINATION_LIMIT_HEADER, limit_string))?,
+    );
+
+    Ok((
         StatusCode::OK,
         headers,
         Json(vec![Item {
@@ -68,7 +101,7 @@ async fn list_items(Query(list_params): Query<ListQueryParams>) -> impl IntoResp
             created_at: Utc::now().naive_utc(),
             updated_at: Utc::now().naive_utc(),
         }]),
-    )
+    ))
 }
 
 async fn create_item(Json(body): Json<UpdateItemBody>) -> impl IntoResponse {
