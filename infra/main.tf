aa62d5371a960e72a469789a53e2456e5fcd717a
ysignat <ignatovegors@gmail.com>
2024-09-21 21:51:52 +0300

    feat(infra): added lifecycle policy for API images

--- infra/main.tf
+++ infra/main.tf
@@ -39,6 +39,25 @@ resource "yandex_container_registry" "default" {
   name = "default"
 }
 
+data "yandex_container_repository" "api" {
+  name = "${yandex_container_registry.default.id}/api"
+}
+
+resource "yandex_container_repository_lifecycle_policy" "api" {
+  name          = "api"
+  status        = "active"
+  description   = "Lifecycle of API images"
+  repository_id = data.yandex_container_repository.api.id
+
+  rule {
+    description   = "Simple policy for beta images"
+    untagged      = false
+    tag_regexp    = "beta.*"
+    retained_top  = 3
+    expire_period = "168h"
+  }
+}
+
 resource "yandex_iam_service_account" "container_registry" {
   name        = "container-registry"
   description = "Service account for shipping docker images from CI"
